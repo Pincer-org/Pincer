1946ce8639f0430553ed8f62c4b3523d4ec3327c
Arthurdw <mail@arthurdw.com>
2021-08-28 21:45:24 +0200

    :rocket: Initial setup.

---
+++ pyscord/__init__.py
@@ -0,0 +1,16 @@
+"""
+Pyscord library
+====================
+An asynchronous python API wrapper meant to replace discord.py
+
+:copyright: (c) 2021-present
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "Pyscord library"
+__package__ = "pyscord"
+__author__ = "Sigmanficient, Arthurdw"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present"
+__version__ = "0.0.0-p"
+__description__ = "Discord API wrapper rebuild from scratch."
--- setup.py
+++ setup.py
@@ -1,30 +1,69 @@
-import pathlib
-from setuptools import setup
+from distutils.core import setup
+from os import path
+from re import search, MULTILINE
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
+this_dir = path.abspath(path.dirname(__file__))
 
-# The text of the README file
-README = (HERE / "README.md").read_text(encoding='utf8')
+with open(path.join(this_dir, "README.md"), encoding='utf-8') as f:
+    dyn_props = {
+        "long_description": f.read(),
+        "long_description_content_type": "text/markdown"
+    }
 
-# This call to setup() does all the work
+with open(path.join(this_dir, "pyscord", "__init__.py"), encoding='utf-8') as f:
+    _re_match_str = r'\s*[\'"]([^\'"]*)[\'"]'
+    content = f.read()
+
+
+    def get_content(name: str):
+        match = search(name + _re_match_str, content, MULTILINE).group(1)
+        if not match:
+            raise RuntimeError(f"{name} is not set!")
+        return match
+
+
+    version = get_content("__version__")
+
+    dyn_props = {
+        **dyn_props,
+        "name": get_content("package"),
+        "version": version,
+        "packages": ["pyscord", "pyscord.utils"],
+        "license": get_content("__license__"),
+        "description": get_content("__description__"),
+        "author": get_content("__author__"),
+        "copyright": get_content("__copyright__")
+    }
+
+with open(path.join(this_dir, "requirements.txt"), encoding='utf-8') as f:
+    dyn_props = {**dyn_props, "install_requires": [itm.strip() for itm in
+                                                   f.read().strip().split(" ")]}
+
 setup(
-    name="pyscord",
-    version="0.0.1",
-    description="Discord API wrapper rebuild from scratch.",
-    long_description=README,
-    long_description_content_type="text/markdown",
+    **dyn_props,
+    project_urls={
+        "Documentation": "-- WIP --",
+    },
     url="https://github.com/Pyscord/Pyscord",
-    author="Pyscord",
     author_email="...",
-    license="MIT",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        # Development statuses:
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
-    packages=['pyscord'],
     include_package_data=True,
-    install_requires=[]
+    keywords=["discord", "api", "asynchronous"],
+    extras_require={},
 )
