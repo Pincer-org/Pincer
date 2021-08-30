07f5ddb154be0786adb940cf65b33b27340fb00f
Endercheif <45527309+Endercheif@users.noreply.github.com>
2021-08-30 11:55:41 -0700

    ✏️ Typo for extention

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import sphinx_rtd_dark_mode
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx-rtd-dark-mode']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx_rtd_dark_mode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
