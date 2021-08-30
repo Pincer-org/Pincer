171fd5502410e2f37ccaebc6460de8439f05901b
Endercheif <45527309+Endercheif@users.noreply.github.com>
2021-08-30 11:15:12 -0700

    🚑️ Added Requirements in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,10 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+with open('../requirements.txt') as f:
+    f.read()
+    autodock_mock_imports = f.read().splitlines()
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
