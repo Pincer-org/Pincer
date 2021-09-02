26e3b8c6f8b447268d602d19b8b582001dda8976
Arthurdw <mail@arthurdw.com>
2021-09-02 13:53:56 +0200

    :bug: Import bugfix

--- pincer/utils/insertion.py
+++ pincer/utils/insertion.py
@@ -21,9 +21,8 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from collections import Callable
 from inspect import getfullargspec
-from typing import Any, Union
+from typing import Any, Union, Callable
 
 from pincer.utils.types import Coro
 
