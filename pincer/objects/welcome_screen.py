2d7f781fe47fcf88d8149a52eef42c2df89aad4c
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-09-04 13:32:19 -0400

    Added Welcome Screen Objects

---
+++ pincer/objects/welcome_screen.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2021 Pincer
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional, List
+
+from pincer.utils.api_object import APIObject
+
+
+@dataclass
+class WelcomeScreenChannel(APIObject):
+    channel_id: int
+    description: str
+
+    emoji_id: Optional[int] = None
+    emoji_name: Optional[str] = None
+
+
+@dataclass
+class WelcomeScreen(APIObject):
+    welcome_channels: WelcomeScreenChannel
+
+    description: Optional[str] = None
