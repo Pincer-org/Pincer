54da46710e2838401636afc3fd8b0b05798dfb34
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-09-04 13:29:23 -0400

    Added Webhook Object

---
+++ pincer/objects/webhook.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2021 Pincer
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+from pincer.objects.channel import Channel
+from pincer.objects.guild import Guild
+from pincer.objects.user import User
+from pincer.utils.api_object import APIObject
+from pincer.utils.constants import OptionallyProvided, MISSING
+
+
+class WebhookType(Enum):
+    INCOMING = 1
+    CHANNEL_FOLLOWER = 2
+    APPLICATION = 3
+
+@dataclass
+class Webhook(APIObject):
+    id: int
+    type: WebhookType
+
+    channel_id: Optional[int] = None
+    name: Optional[str] = None
+    avatar: Optional[str] = None
+    application_id: Optional[int] = None
+
+    user: OptionallyProvided[User] = MISSING
+    token: OptionallyProvided[str] = MISSING
+    source_guild: OptionallyProvided[Guild] = MISSING
+    source_channel: OptionallyProvided[Channel] = MISSING
+    url: OptionallyProvided[str] = MISSING
+    
+    guild_id: OptionallyProvided[Optional[int]] = MISSING
