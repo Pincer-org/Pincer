6cbab8476b2ec768747188d9e6777c8b6aa1667a
endercheif <reet22singh@gmail.com>
2021-09-04 17:42:51 -0700

    🔧Default lightmode

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ with open('../requirements.txt') as f:
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-default_dark_mode = True
+default_dark_mode = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
