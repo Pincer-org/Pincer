c15d2b655a08969bc6acfbdfd628b28209579dc7
trag1c <xyakoo@yahoo.com>
2021-09-05 02:26:38 +0200

    📝 Added documentation

--- pincer/objects/attachment.py
+++ pincer/objects/attachment.py
@@ -31,6 +31,17 @@ from pincer.utils.snowflake import Snowflake
 
 @dataclass
 class Attachment(APIObject):
+	"""Represents a Discord Attachment object
+	
+	:param id: attachment id
+	:param filename: name of file attached
+	:param content_type: the attachment's data type
+	:param size: size of file in bytes
+	:param url: source url of file
+	:param proxy_url: a proxied url of file
+	:param height: height of file (if image)
+	:param width: width of file (if image)
+	"""
 	id: Snowflake
 	filename: str
 	size: int
