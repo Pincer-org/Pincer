5993ec3ddfb1109bb47a54c330767e3730f72fc8
trag1c <xyakoo@yahoo.com>
2021-09-05 15:55:16 +0200

    📝 Improved documentation

--- pincer/objects/guild_template.py
+++ pincer/objects/guild_template.py
@@ -36,17 +36,39 @@ class GuildTemplate(APIObject):
     """Represents a code that when used,
     creates a guild based on a snapshot of an existing guild.
 
-    :param code: the template code (unique ID)
-    :param name: template name
-    :param description: the description for the template
-    :param usage_count: number of times this template has been used
-    :param creator_id: the ID of the user who created the template
-    :param creator: the user who created the template
-    :param created_at: when this template was created
-    :param updated_at: when this template was last synced to the source guild
-    :param source_guild_id: the ID of the guild this template is based on
-    :param serialized_source_guild: the guild snapshot this template contains
-    :param is_dirty: whether the template has unsynced changes
+
+    :param code:
+        the template code (unique ID)
+
+    :param name:
+        template name
+
+    :param description:
+        the description for the template
+
+    :param usage_count:
+        number of times this template has been used
+
+    :param creator_id:
+        the ID of the user who created the template
+
+    :param creator: the
+        user who created the template
+
+    :param created_at:
+        when this template was created
+
+    :param updated_at:
+        when this template was last synced to the source guild
+
+    :param source_guild_id:
+        the ID of the guild this template is based on
+
+    :param serialized_source_guild:
+        the guild snapshot this template contains
+
+    :param is_dirty:
+        whether the template has unsynced changes
     """
     code: str
     name: str
--- pincer/objects/voice_state.py
+++ pincer/objects/voice_state.py
@@ -35,19 +35,45 @@ from pincer.utils.timestamp import Timestamp
 class VoiceState(APIObject):
     """Used to represent a user's voice connection status
 
-    :param guild_id: the guild id this voice state is for
-    :param channel_id: the channel id this user is connected to
-    :param user_id: the user id this voice state is for
-    :param member: the guild member this voice state is for
-    :param session_id: the session id for this voice state
-    :param deaf: whether this user is deafened by the server
-    :param mute: whether this user is muted by the server
-    :param self_deaf: whether this user is locally deafened
-    :param self_mute: whether this user is locally muted
-    :param self_stream: whether this user is streaming using "Go Live"
-    :param self_video: whether this user's camera is enabled
-    :param suppress: whether this user is muted by the current user
-    :param request_to_speak_timestamp: the time at which the user requested to speak
+
+    :param guild_id:
+		the guild id this voice state is for
+
+    :param channel_id:
+		the channel id this user is connected to
+
+    :param user_id:
+		the user id this voice state is for
+
+    :param member:
+		the guild member this voice state is for
+
+    :param session_id:
+		the session id for this voice state
+
+    :param deaf:
+		whether this user is deafened by the server
+
+    :param mute:
+		whether this user is muted by the server
+
+    :param self_deaf:
+		whether this user is locally deafened
+
+    :param self_mute:
+		whether this user is locally muted
+
+    :param self_stream:
+		whether this user is streaming using "Go Live"
+
+    :param self_video:
+		whether this user's camera is enabled
+
+    :param suppress:
+		whether this user is muted by the current user
+
+    :param request_to_speak_timestamp:
+		the time at which the user requested to speak
     """
     channel_id: Optional[Snowflake]
     user_id: Snowflake
