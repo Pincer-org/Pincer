0ee0625d7fa70bf3c9c347f00b27bfd8af911db9
Sly-Little-Fox <69681150+Sly-Little-Fox@users.noreply.github.com>
2021-09-08 21:20:27 +0300

    🔧 Fix a typo
    Rebuild => rebuilt

--- pincer/__init__.py
+++ pincer/__init__.py
@@ -12,7 +12,7 @@ __package__ = "pincer"
 __author__ = "Sigmanificient, Arthurdw"
 __license__ = "MIT"
 __version__ = "0.4.1-dev"
-__description__ = "Discord API wrapper rebuild from scratch."
+__description__ = "Discord API wrapper rebuilt from scratch."
 
 from .client import Client, Bot
 
