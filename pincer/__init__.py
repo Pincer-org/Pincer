90afa633f31ace30f839a4dba01f9027514ccb4e
Arthurdw <mail@arthurdw.com>
2021-09-10 15:43:11 +0200

    :bookmark: Version 0.6.3-dev

--- pincer/__init__.py
+++ pincer/__init__.py
@@ -11,7 +11,7 @@ __title__ = "Pincer library"
 __package__ = "pincer"
 __author__ = "Sigmanificient, Arthurdw"
 __license__ = "MIT"
-__version__ = "0.6.2-dev"
+__version__ = "0.6.3-dev"
 __description__ = "Discord API wrapper rebuild from scratch."
 
 from pincer.client import Client, Bot
