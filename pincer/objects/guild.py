d74c68c07a974d58472c35e157c0b2c0dbe035ab
trag1c <xyakoo@yahoo.com>
2021-09-10 17:27:29 +0200

    PR#60

--- pincer/objects/guild.py
+++ pincer/objects/guild.py
@@ -24,208 +24,19 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from enum import Enum, auto, IntEnum
 from typing import Optional, List
 
 from ..exceptions import UnavailableGuildError
 from .channel import Channel
 from .emoji import Emoji
 from .guild_member import GuildMember
-from .presence import PresenceUpdateEvent
+from .guild_features import GuildFeatures
 from .role import Role
 from .stage import StageInstance
 from .sticker import Sticker
-from .voice_state import VoiceState
 from .welcome_screen import WelcomeScreen
 from ..utils import APIObject, APINullable, MISSING, Snowflake, Timestamp
 
-class PremiumTier(IntEnum):
-    """
-    :param NONE:
-        guild has not unlocked any Server Boost perks
-
-    :param TIER_1:
-        guild has unlocked Server Boost level 1 perks
-
-    :param TIER_2:
-        guild has unlocked Server Boost level 2 perks
-
-    :param TIER_3:
-        guild has unlocked Server Boost level 3 perks
-    """
-    NONE = 0
-    TIER_1 = 1
-    TIER_2 = 2
-    TIER_3 = 3
-
-class GuildNSFWLevel(IntEnum):
-    DEFAULT = 0
-    EXPLICIT = 1
-    SAFE = 2
-    AGE_RESTRICTED = 3
-
-class ExplicitContentFilterLevel(IntEnum):
-    """
-    :param DISABLED:
-        media content will not be scanned
-
-    :param MEMBERS_WITHOUT_ROLES:
-        media content sent by members without roles will be scanned
-
-    :param ALL_MEMBERS:
-        media content sent by all members will be scanned
-    """
-    DISABLED = 0
-    MEMBERS_WITHOUT_ROLES = 1
-    ALL_MEMBERS = 2
-
-class MFALevel(IntEnum):
-    """
-    :param NONE:
-        guild has no MFA/2FA requirement for moderation actions
-
-    :param ELEVATED:
-        guild has a 2FA requirement for moderation actions
-    """
-    NONE = 0
-    ELEVATED = 1
-
-class VerificationLevel(IntEnum):
-    """
-    :param NONE:
-        unrestricted
-
-    :param LOW:
-        must have verified email on account
-
-    :param MEDIUM:
-        must be registered on Discord for longer than 5 minutes
-
-    :param HIGH:
-        must be a member of the server for longer than 10 minutes
-
-    :param VERY_HIGH:
-        must have a verified phone number
-    """
-    NONE = 0
-    LOW = 1
-    MEDIUM = 2
-    HIGH = 3
-    VERY_HIGH = 4
-
-class DefaultMessageNotificationLevel(IntEnum):
-    """
-    :param ALL_MESSAGES:
-        members will receive notifications for all messages by default
-
-    :param ONLY_MENTIONS:
-        members will receive notifications only
-        for messages that @mention them by default
-    """
-    ALL_MESSAGES = 0
-    ONLY_MENTIONS = 1
-
-class SystemChannelFlags(IntEnum):
-    """
-    :param SUPPRESS_JOIN_NOTIFICATIONS:
-        Suppress member join notifications
-
-    :param SUPPRESS_PREMIUM_SUBSCRIPTIONS:
-        Suppress server boost notifications
-
-    :param SUPPRESS_GUILD_REMINDER_NOTIFICATIONS:
-        Suppress server setup tips
-    """
-    SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0
-    SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1
-    SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2
-
-class GuildFeature(Enum):
-    """
-    :param ANIMATED_ICON:
-        guild has access to set an animated guild icon
-
-    :param BANNER:
-        guild has access to set a guild banner image
-
-    :param COMMERCE:
-        guild has access to use commerce features (i.e. create store channels)
-
-    :param COMMUNITY:
-        guild can enable welcome screen, Membership Screening, stage channels and discovery, and receives community updates
-
-    :param DISCOVERABLE:
-        guild is able to be discovered in the directory
-
-    :param FEATURABLE:
-        guild is able to be featured in the directory
-
-    :param INVITE_SPLASH:
-        guild has access to set an invite splash background
-
-    :param MEMBER_VERIFICATION_GATE_ENABLED:
-        guild has enabled Membership Screening
-
-    :param NEWS:
-        guild has access to create news channels
-
-    :param PARTNERED:
-        guild is partnered
-
-    :param PREVIEW_ENABLED:
-        guild can be previewed before joining via Membership Screening or the directory
-
-    :param VANITY_URL:
-        guild has access to set a vanity URL
-
-    :param VERIFIED:
-        guild is verified
-
-    :param VIP_REGIONS:
-        guild has access to set 384kbps bitrate in voice (previously VIP voice servers)
-
-    :param WELCOME_SCREEN_ENABLED:
-        guild has enabled the welcome screen
-
-    :param TICKETED_EVENTS_ENABLED:
-        guild has enabled ticketed events
-
-    :param MONETIZATION_ENABLED:
-        guild has enabled monetization
-
-    :param MORE_STICKERS:
-        guild has increased custom sticker slots
-
-    :param THREE_DAY_THREAD_ARCHIVE:
-        guild has access to the three day archive time for threads
-
-    :param SEVEN_DAY_THREAD_ARCHIVE:
-        guild has access to the seven day archive time for threads
-
-    :param PRIVATE_THREADS:
-        guild has access to create private threads
-    """
-    ANIMATED_ICON = auto()
-    BANNER = auto()
-    COMMERCE = auto()
-    COMMUNITY = auto()
-    DISCOVERABLE = auto()
-    FEATURABLE = auto()
-    INVITE_SPLASH = auto()
-    MEMBER_VERIFICATION_GATE_ENABLED = auto()
-    NEWS = auto()
-    PARTNERED = auto()
-    PREVIEW_ENABLED = auto()
-    VANITY_URL = auto()
-    VERIFIED = auto()
-    VIP_REGIONS = auto()
-    WELCOME_SCREEN_ENABLED = auto()
-    TICKETED_EVENTS_ENABLED = auto()
-    MONETIZATION_ENABLED = auto()
-    MORE_STICKERS = auto()
-    THREE_DAY_THREAD_ARCHIVE = auto()
-    SEVEN_DAY_THREAD_ARCHIVE = auto()
-    PRIVATE_THREADS = auto()
 
 @dataclass
 class Guild(APIObject):
@@ -398,30 +209,30 @@ class Guild(APIObject):
 
     afk_channel_id: Optional[Snowflake]
     afk_timeout: int
-    application_id: Optional[Snowflake]
+    application_id: Optional[int]
     banner: Optional[str]
-    default_message_notifications: DefaultMessageNotificationLevel
+    default_message_notifications: int
     description: Optional[str]
     discovery_splash: Optional[str]
     emojis: List[Emoji]
-    explicit_content_filter: ExplicitContentFilterLevel
-    features: List[GuildFeature]
+    explicit_content_filter: int
+    features: List[GuildFeatures]
     id: Snowflake
     icon: Optional[str]
-    mfa_level: MFALevel
+    mfa_level: int
     name: str
-    nsfw_level: GuildNSFWLevel
+    nsfw_level: int
     owner_id: Snowflake
     preferred_locale: str
-    premium_tier: PremiumTier
+    premium_tier: int
     public_updates_channel_id: Optional[Snowflake]
     roles: List[Role]
-    rules_channel_id: Optional[Snowflake]
+    rules_channel_id: Optional[int]
     splash: Optional[str]
-    system_channel_flags: SystemChannelFlags
-    system_channel_id: Optional[Snowflake]
+    system_channel_flags: int
+    system_channel_id: Optional[int]
     vanity_url_code: Optional[str]
-    verification_level: VerificationLevel
+    verification_level: int
 
     approximate_member_count: APINullable[int] = MISSING
     approximate_presence_count: APINullable[int] = MISSING
@@ -437,14 +248,14 @@ class Guild(APIObject):
     owner: APINullable[bool] = MISSING
     permissions: APINullable[str] = MISSING
     premium_subscription_count: APINullable[int] = MISSING
-    presences: APINullable[List[PresenceUpdateEvent]] = MISSING
+    presences: APINullable[List[...]] = MISSING
     stage_instances: APINullable[List[StageInstance]] = MISSING
     stickers: APINullable[List[Sticker]] = MISSING
     region: APINullable[Optional[str]] = MISSING
     threads: APINullable[List[Channel]] = MISSING
     # Guilds are considered available unless otherwise specified
     unavailable: APINullable[bool] = False
-    voice_states: APINullable[List[VoiceState]] = MISSING
+    voice_states: APINullable[bool] = MISSING
     widget_enabled: APINullable[bool] = MISSING
     widget_channel_id: APINullable[Optional[Snowflake]] = MISSING
     welcome_screen: APINullable[WelcomeScreen] = MISSING
