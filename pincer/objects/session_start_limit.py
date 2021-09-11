825653000b8e4511f20bfa40f46dc0ed68e1b8b4
trag1c <xyakoo@yahoo.com>
2021-09-11 02:42:48 +0200

    🎨 Changed indentation to spaces

--- pincer/objects/session_start_limit.py
+++ pincer/objects/session_start_limit.py
@@ -28,22 +28,22 @@ from ..utils.api_object import APIObject
 
 @dataclass
 class SessionStartLimit(APIObject):
-	"""
-	Represents a Discord Session Start Limit object
+    """
+    Represents a Discord Session Start Limit object
 
-	:param total:
-		The total number of session starts the current user is allowed
+    :param total:
+        The total number of session starts the current user is allowed
 
-	:param remaining:
-		The remaining number of session starts the current user is allowed
+    :param remaining:
+        The remaining number of session starts the current user is allowed
 
-	:param reset_after:
-		The number of milliseconds after which the limit resets
+    :param reset_after:
+        The number of milliseconds after which the limit resets
 
-	:param max_concurrency:
-		The number of identify requests allowed per 5 seconds
-	"""
-	total: int
-	remaining: int
-	reset_after: int
-	max_concurrency: int
+    :param max_concurrency:
+        The number of identify requests allowed per 5 seconds
+    """
+    total: int
+    remaining: int
+    reset_after: int
+    max_concurrency: int
