f81e311607ffea82217ee56a138003c261c59086
Yohann Boniface <edhyjox@gmail.com>
2021-09-11 15:44:59 +0200

    :sparkles: Added embed example

--- examples/chat_commands_class_based.py
+++ examples/chat_commands_class_based.py
@@ -20,6 +20,27 @@ class Bot(Client):
     async def private_say(self, message: str):
         return Message(message, flags=InteractionFlags.EPHEMERAL)
 
+    @command(description="How to make embed!")
+    async def pincer_embed(self):
+        return Embed(
+            title="Pincer - 0.6.4",
+            description=(
+                "🚀 An asynchronous python API wrapper meant to replace"
+                " discord.py\n> Snappy discord api wrapper written "
+                "with aiohttp & websockets"
+            )
+        ).add_field(
+            name="**Github Repository**",
+            value="> https://github.com/Pincer-org/Pincer"
+        ).set_thumbnail(
+            url="https://pincer.dev/img/icon.png"
+        ).set_image(
+            url=(
+                "https://repository-images.githubusercontent.com"
+                "/400871418/045ebf39-7c6e-4c3a-b744-0c3122374203"
+            )
+        )
+
 
 if __name__ == "__main__":
     Bot("XXXYOURBOTTOKENHEREXXX").run()
