34d7f7a5671a8aeb5b323444bb719a7742ad4615
endercheif <reet22singh@gmail.com>
2021-09-16 19:55:46 -0700

    ✨Guild Object with Client.get_guild

--- pincer/middleware/__init__.py
+++ pincer/middleware/__init__.py
@@ -48,6 +48,7 @@ def get_middleware() -> Dict[str, Coro]:
                 "export"
             )()
         except AttributeError:
+            continue # TODO: Fix this. Always raises error because some modules are empty 
             raise NoExportMethod(
                 f"Middleware module `{middleware_path}` expected an "
                 "`export` method but none was found!"
