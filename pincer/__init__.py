b1862e7e66f912ad5e743cc58a6f191d900c6b0f
BONIFACE <contact@yohannboniface.dev>
2021-09-20 10:57:26 +0200

    🔖 0.6.10

--- pincer/__init__.py
+++ pincer/__init__.py
@@ -11,7 +11,7 @@ __title__ = "Pincer library"
 __package__ = "pincer"
 __author__ = "Sigmanificient, Arthurdw"
 __license__ = "MIT"
-__version__ = "0.7.0-dev"
+__version__ = "0.6.10-dev"
 __description__ = "Discord API wrapper rebuild from scratch."
 
 from pincer.client import Client, Bot
