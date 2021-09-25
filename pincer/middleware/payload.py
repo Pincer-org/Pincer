38f9ead76a3c8ea22968f7f197883f3d86020844
Sly-Little-Fox <fox@sly-little-fox.ru>
2021-09-25 17:19:10 +0300

    🔧 Fix Scrutinizer warnings

--- pincer/middleware/payload.py
+++ pincer/middleware/payload.py
@@ -1,14 +1,16 @@
 # Copyright Pincer 2021-Present
 # Full MIT License can be found in `LICENSE` at the project root.
 
-"""sent when basically anything is received from gateway"""
-
 from ..core.dispatch import GatewayDispatch
 
 
-async def payload_middleware(self, payload: GatewayDispatch):
+async def payload_middleware(payload: GatewayDispatch):
+    """Invoked when basically anything is received from gateway.."""
+
     return "on_payload", payload
 
 
 def export():
+    """Export the middleware"""
+
     return payload_middleware
