75f564117f8608e1b73bc37e81b51166f6ffd498
Yohann Boniface <edhyjox@gmail.com>
2021-09-26 22:47:27 +0200

    :sparkles: Using `VersionInfo` class & adding to `__all__`

--- pincer/__init__.py
+++ pincer/__init__.py
@@ -7,15 +7,37 @@ Copyright Pincer 2021
 Full MIT License can be found in `LICENSE` at the project root.
 """
 
-__title__ = "Pincer library"
-__package__ = "pincer"
-__author__ = "Sigmanificient, Arthurdw"
-__license__ = "MIT"
-__version__ = "0.6.11-dev"
-__description__ = "Discord API wrapper rebuild from scratch."
+from typing import NamedTuple, Literal
 
 from pincer.client import Client, Bot
 from pincer.commands import command
 from pincer.objects import Intents
 
-__all__ = ("Client", "Bot", "command", "Intents")
+__package__ = "pincer"
+__title__ = "Pincer library"
+__description__ = "Discord API wrapper rebuild from scratch."
+__author__ = "Sigmanificient, Arthurdw"
+__email__ = "contact@pincer.org"
+__license__ = "MIT"
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release_level: Literal["alpha", "beta", "candidate", "final", "dev"]
+    serial: int
+
+    def __repr__(self) -> str:
+        return (
+            f'{self.major}.{self.minor}.{self.micro}'
+            f'-{self.release_level}{self.serial}'
+        )
+
+
+__version__ = VersionInfo(0, 6, 11, 'dev', 0)
+__all__ = (
+    "__version__", "__title__", "__package__",
+    "__author__", "__email__",
+    "Client", "Bot", "command", "Intents"
+)
