ef66167961fd91573dd605104aaf8fc0d80b3411
Yohann Boniface <edhyjox@gmail.com>
2021-09-28 23:38:03 +0200

    :sparkles: `channel_create` middleware

--- pincer/middleware/channel_create.py
+++ pincer/middleware/channel_create.py
@@ -2,5 +2,22 @@
 # Full MIT License can be found in `LICENSE` at the project root.
 
 """sent when a channel is created/joined on the client"""
+from pincer.core.dispatch import GatewayDispatch
+from pincer.objects import Channel
 
-# TODO: Implement event
+
+def channel_create_middleware(self, payload: GatewayDispatch):
+    """
+    Middleware for ``on_error`` event.
+
+    :param client:
+
+    :param payload:
+        The data received from the ready event.
+    """
+
+    return "on_channel_creation",  [
+        Channel.from_dict(
+            {"_client": self, "_http": self.http, **payload.data}
+        )
+    ]
