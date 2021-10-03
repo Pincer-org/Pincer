04e18cb4eae94d255fc1c04ec23b5bce25cef4e6
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-03 21:24:07 +0100

    :wrench: Change config for ext's and themes

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-
+sys.path.append(os.path.abspath('extensions'))
 
 # -- Project information -----------------------------------------------------
 from pincer import __title__, __author__, __version__
@@ -25,11 +25,12 @@ author = __author__
 # The full version, including alpha/beta/rc tags
 release = __version__
 
+branch = 'master' if __version__.endswith('a') else 'v' + __version__
 
 # -- General configuration ---------------------------------------------------
 
-import sphinx_rtd_theme
-import sphinx_rtd_dark_mode
+import furo
+import sphinx_autodoc_typehints
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,9 +38,21 @@ import sphinx_rtd_dark_mode
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
-    'sphinx_rtd_dark_mode'
+    'sphinx.ext.napoleon',
+    'exception_hierarchy',
+    'attributetable',
+    'silence',
+    'sphinx_autodoc_typehints'
 ]
+
+autodoc_typehints = 'none'
+
+resource_links = {
+    'discord': 'https://discord.gg/et54DgVjMX',
+    'issues': 'https://github.com/Pincer-org/Pincer/issues',
+    'discussions': 'https://github.com/Pincer-org/Pincer/discussions',
+    'examples': f'https://github.com/Pincer-org/Pincer/tree/{branch}/examples',
+}
 
 intersphinx_mapping = {
     'py': ('https://docs.python.org/3', None),
@@ -47,7 +60,7 @@ intersphinx_mapping = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -64,10 +77,35 @@ with open('../requirements.txt') as f:
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_logo = '../assets/img/icon_smol.png'
+html_theme = 'furo'
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+    'light_css_variables': {
+        'color-brand-primary': '#4C8CBF',
+        'color-brand-content': '#306998',
+        'color-admonition-background': 'blue',
+    },
+    'dark_css_variables': {
+        'color-brand-primary': '#306998',
+        'color-brand-content': '#FFE871',
+        'color-admonition-background': 'yellow',
+    },
+    "sidebar_hide_name": True,
+}
 pygments_style = 'monokai'
-default_dark_mode = False
+default_dark_mode = True
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+# html_static_path = ['_static']
+
+rst_prolog = '''
+.. |coro| replace:: This function is a |coroutine_link|_.
+.. |maybecoro| replace:: This function *could be a* |coroutine_link|_.
+.. |coroutine_link| replace:: *coroutine*
+.. _coroutine_link: https://docs.python.org/3/library/asyncio-task.html#coroutine
+'''
+
+# The suffix of source filenames.
+source_suffix = '.rst'
