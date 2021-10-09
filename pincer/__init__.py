da750a264f91360e5677a0c8c97e4fd0e1e7c7a8
Yohann Boniface <edhyjox@gmail.com>
2021-10-09 20:36:42 +0200

    🔖 Bumping to 0.9.1

--- pincer/__init__.py
+++ pincer/__init__.py
@@ -44,7 +44,7 @@ class VersionInfo(NamedTuple):
         )
 
 
-__version__ = VersionInfo(0, 8, 0)
+__version__ = VersionInfo(0, 9, 1)
 __all__ = (
     "Bot", "Choices", "Client", "Intents", "__author__", "__email__",
     "__package__", "__title__", "__version__", "command"
