b8a403063161dc68fe2d44948ef6829e89027ee0
Lunarmagpie <Bambolambo0@gmail.com>
2021-10-09 23:00:51 -0400

    🔀 merge Lunarmagpie/Pincer with main branch.

--- pincer/objects/app/interactions.py
+++ pincer/objects/app/interactions.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import IntEnum
-from logging import NOTSET
 from typing import Dict, TYPE_CHECKING
 
 import asyncio
