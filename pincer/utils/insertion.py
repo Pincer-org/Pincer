10770f593433bfa8ea6e83d4b619af706d1b103a
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-17 21:46:56 +0100

    :memo: Not Doc utils.insertion

--- pincer/utils/insertion.py
+++ pincer/utils/insertion.py
@@ -9,15 +9,6 @@ from ..objects.message.context import MessageContext
 
 
 def should_pass_cls(call: Union[Coro, Callable[..., Any]]) -> bool:
-    """
-    Checks whether a callable requires a self/cls as first parameter.
-
-    :param call:
-        The callable to check.
-
-    :return:
-        Whether or not its required.
-    """
     args = getfullargspec(call).args
     return len(args) >= 1 and args[0] in ["self", "cls"]
 
@@ -26,5 +17,5 @@ context_types = [Signature.empty, MessageContext]
 
 
 def should_pass_ctx(sig: Mapping[str, Parameter], params: List[str]) -> bool:
-    # TODO: Write docs
+    # ? this is an internal function lmao
     return len(params) >= 1 and sig[params[0]].annotation in context_types
