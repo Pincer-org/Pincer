efeb6dedffa6f106bf8afe16911e9d1ebee47fb3
Endercheif <reet22singh@gmail.com>
2021-10-23 22:22:51 -0700

    ✨Added thread delete

--- pincer/middleware/thread_delete.py
+++ pincer/middleware/thread_delete.py
@@ -1,28 +1,28 @@
 # Copyright Pincer 2021-Present
 # Full MIT License can be found in `LICENSE` at the project root.
 
-"""sent when a channel is deleted"""
+"""sent when a thread is deleted"""
 
 from ..core.dispatch import GatewayDispatch
 from ..objects import Channel
 from ..utils.conversion import construct_client_dict
 
 
-async def channel_delete_middleware(self, payload: GatewayDispatch):
+async def thread_delete_middleware(self, payload: GatewayDispatch):
     """
-    Middleware for ``on_channel_delete``,
-        creates a object for the channel that is deleted
+    Middleware for ``on_thread_delete``,
+        creates a object for the thread that is deleted
 
     :param self:
         The current client.
 
     :param payload:
-        The data received from the channel delete event.
+        The data received from the thread delete event.
     """
-    return "on_channel_delete", [
+    return "on_thread_delete", [
         Channel.from_dict(construct_client_dict(self, payload.data))
     ]
 
 
 def export():
-    return channel_delete_middleware
+    return thread_delete_middleware
