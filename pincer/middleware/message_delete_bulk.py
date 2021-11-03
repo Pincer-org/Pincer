7e0c3e2a25f7abcac1f22f50e4802d89d4ab8e15
beastmatser <79206232+beastmatser@users.noreply.github.com>
2021-11-03 12:20:11 +0100

    :sparkles: Added middleware for message_delete_bulk

---
+++ pincer/middleware/message_delete_bulk.py
@@ -0,0 +1,37 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""sent when multiple messages are deleted at once"""
+
+from pincer.utils.types import Coro
+from ..core.dispatch import GatewayDispatch
+from ..objects.events.message import MessageDeleteBulkEvent
+from ..utils.conversion import construct_client_dict
+
+
+async def message_delete_bulk_middleware(self, payload: GatewayDispatch):
+    """|coro|
+
+    Middleware for ``on_message_delete_bulk``,
+
+    Paramaters
+    ----------
+
+    :param self:
+        The current client.
+
+    :param payload:
+        The data received from the message delete bulk event.
+
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`~pincer.events.message.MessageDeleteBulkEvent`]]
+        ``on_message_delete_bulk`` and an ``MessageDeleteBulkEvent`` object
+    """
+    return "on_message_delete_bulk", [
+        MessageDeleteBulkEvent.from_dict(construct_client_dict(self, payload.data))
+    ]
+
+
+def export() -> Coro:
+    return message_delete_bulk_middleware
