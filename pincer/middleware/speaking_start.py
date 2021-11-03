b6f298bf1294fabc53f1ae2b21c01998f03d881d
beastmatser <79206232+beastmatser@users.noreply.github.com>
2021-11-03 11:55:47 +0100

    :sparkles: Added speaking_start and stop middleware

--- pincer/middleware/speaking_start.py
+++ pincer/middleware/speaking_start.py
@@ -3,4 +3,31 @@
 
 """sent when a user in a subscribed voice channel speaks"""
 
-# TODO: Implement event
+from ..core.dispatch import GatewayDispatch
+from ..utils.snowflake import Snowflake
+from ..utils.types import Coro
+
+
+async def speaking_start_middleware(self, payload: GatewayDispatch):
+    """|coro|
+    
+    Middleware for ``on_speaking_start`` event.
+    
+    Parameters
+    ----------
+    self : :class:`Client`
+        The current client/bot.
+    payload : :class:`GatewayDispatch`
+        The data received from the speaking start event.
+        
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`Snowflake`]]
+        ``on_speaking_start`` and a ``Snowflake`` (user_id)
+    """
+    user_id: Snowflake = Snowflake.from_string(payload.data.get("user_id"))
+    return "on_speaking_start", [user_id]
+
+
+def export() -> Coro:
+    return speaking_start_middleware
--- pincer/middleware/speaking_stop.py
+++ pincer/middleware/speaking_stop.py
@@ -3,4 +3,31 @@
 
 """sent when a user in a subscribed voice channel stops speaking"""
 
-# TODO: Implement event
+from ..core.dispatch import GatewayDispatch
+from ..utils.snowflake import Snowflake
+from ..utils.types import Coro
+
+
+async def speaking_stop_middleware(self, payload: GatewayDispatch):
+    """|coro|
+    
+    Middleware for ``on_speaking_stop`` event.
+    
+    Parameters
+    ----------
+    self : :class:`Client`
+        The current client/bot.
+    payload : :class:`GatewayDispatch`
+        The data received from the speaking stop event.
+        
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`Snowflake`]]
+        ``on_speaking_stop`` and a ``Snowflake`` (user_id)
+    """
+    user_id: Snowflake = Snowflake.from_string(payload.data.get("user_id"))
+    return "on_speaking_stop", [user_id]
+
+
+def export() -> Coro:
+    return speaking_stop_middleware
