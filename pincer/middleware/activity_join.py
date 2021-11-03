9a07daa2c581b8e6e00f6cbb478b1e27857f30f3
beastmatser <79206232+beastmatser@users.noreply.github.com>
2021-11-03 11:53:28 +0100

    :sparkles: Added middleware for activity_join_request, join and spectate

--- pincer/middleware/activity_join.py
+++ pincer/middleware/activity_join.py
@@ -6,4 +6,32 @@ Sent when the user clicks a Rich Presence join invite in chat
 to join a game.
 """
 
-# TODO: Implement event
+from ..core.dispatch import GatewayDispatch
+from ..utils.types import Coro
+
+
+async def activity_join_middleware(self, payload: GatewayDispatch):
+    """|coro|
+
+    Middleware for ``on_activity_join`` event.
+
+    Parameters
+    ----------
+    self : :class:`Client`
+        The current client/bot.
+
+    payload : :class:`GatewayDispatch`
+        The data received from the activity join event.
+
+
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`str`]]
+        ``on_activity_join`` and a ``secret``
+    """
+    secret: str = payload.data.get("secret")
+    return "on_activity_join", [secret]
+
+
+def export() -> Coro:
+    return activity_join_middleware
--- pincer/middleware/activity_spectate.py
+++ pincer/middleware/activity_spectate.py
@@ -6,5 +6,31 @@ sent when the user clicks a Rich Presence spectate invite in chat to
 spectate a game
 """
 
-# TODO: Implement event
 
+from ..core.dispatch import GatewayDispatch
+from ..utils.types import Coro
+
+
+async def activity_spectate_middleware(self, payload: GatewayDispatch):
+    """|coro|
+    
+    Middleware for ``on_activity_spectate`` event.
+    
+    Parameters
+    ----------
+    self : :class:`Client`
+        The current client/bot.
+    payload : :class:`GatewayDispatch`
+        The data received from the activity spectate event.
+        
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`str`]]
+        ``on_activity_spectate`` and a ``secret``
+    """
+    secret: str = payload.data.get("secret")
+    return "on_activity_spectate", [secret]
+
+
+def export() -> Coro:
+    return activity_spectate_middleware
