1b4794a928574cd1fe42c8fbf06132407ef30643
trag1c <xyakoo@yahoo.com>
2021-11-16 21:28:06 +0100

    Fixed Guild.kick & Guild.ban

--- pincer/objects/guild/guild.py
+++ pincer/objects/guild/guild.py
@@ -440,20 +440,7 @@ class Guild(APIObject):
         )
         return GuildMember.from_dict(construct_client_dict(self._client, data))
 
-    async def kick(self, member_id: int, **kwargs):
-        """|coro|
-        Kicks a guild member.
-
-        Parameters
-        ----------
-        member_id : :class: int
-            ID of the guild member to kick.
-        \\*\\* kwargs
-            Additional keyword arguments to kick the guild member with.
-        """
-        await self._http.put(f"/guilds/{self.id}/bans/{member_id}", data=kwargs)
-
-    async def ban(self, member_id: int):
+    async def ban(self, member_id: int, **kwargs):
         """|coro|
         Bans a guild member.
 
@@ -461,6 +448,19 @@ class Guild(APIObject):
         ----------
         member_id : :class: int
             ID of the guild member to ban.
+        \\*\\* kwargs
+            Additional keyword arguments to ban the guild member with.
+        """
+        await self._http.put(f"/guilds/{self.id}/bans/{member_id}", data=kwargs)
+
+    async def kick(self, member_id: int):
+        """|coro|
+        Kicks a guild member.
+
+        Parameters
+        ----------
+        member_id : :class: int
+            ID of the guild member to kick.
         """
         await self._http.delete(f"/guilds/{self.id}/members/{member_id}")
 
