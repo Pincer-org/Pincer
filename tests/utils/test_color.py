edcb99a7f1f1489ceea8b061690ec5ac6d737819
sigmanificient <sigmanificient@gmail.com>
2021-11-20 19:38:04 +0100

    :art: Codacy improvements

--- tests/utils/test_color.py
+++ tests/utils/test_color.py
@@ -4,14 +4,16 @@ from pincer.utils import Color
 
 class TestSnowflake:
 
-    def test_conversion(self):
+    @staticmethod
+    def test_conversion():
         c = Color("#aabb01")
         assert c.r == 170
         assert c.g == 187
         assert c.b == 1
         assert c.hex == 'aabb01'
 
-    def test_invalids(self):
+    @staticmethod
+    def test_invalids():
         with pytest.raises(ValueError):
             Color(-1)
             Color(16777216)
