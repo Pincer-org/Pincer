2f1cec377d8fd7fc6ff9bb37ec3dc91c9b25c24a
sigmanificient <sigmanificient@gmail.com>
2021-11-20 19:11:08 +0100

    :white_check_mark: fixing color test

--- tests/utils/test_color.py
+++ tests/utils/test_color.py
@@ -4,7 +4,7 @@ from pincer.utils import Color
 class TestSnowflake:
 
     def test_conversion(self):
-        c = Color("aabbcc")
+        c = Color("#aabbcc")
         assert c.r == 170
         assert c.g == 187
         assert c.b == 204
