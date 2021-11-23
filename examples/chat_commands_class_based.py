5aecb68d3cf7718ed41d9a02cc35823b5931965f
sigmanificient <sigmanificient@gmail.com>
2021-11-24 00:11:00 +0100

    :art: Using black formatting on examples

--- examples/chat_commands_class_based.py
+++ examples/chat_commands_class_based.py
@@ -17,34 +17,40 @@ class Bot(Client):
 
     @command(description="Add two numbers!")
     async def add(
-            self,
-            first: CommandArg[int, Description["The first number"]],
-            second: CommandArg[int, Description["The second number"]]
+        self,
+        first: CommandArg[int, Description["The first number"]],
+        second: CommandArg[int, Description["The second number"]],
     ):
         return f"The addition of `{first}` and `{second}` is `{first + second}`"
 
     @command(guild=1324567890)
-    async def private_say(self, message: CommandArg[str, Description["The content of the message"]]):
+    async def private_say(
+        self,
+        message: CommandArg[str, Description["The content of the message"]],
+    ):
         return Message(message, flags=InteractionFlags.EPHEMERAL)
 
     @command(description="How to make embed!")
     async def pincer_embed(self):
-        return Embed(
-            title="Pincer - 0.6.4",
-            description=(
-                "🚀 An asynchronous python API wrapper meant to replace"
-                " discord.py\n> Snappy discord api wrapper written "
-                "with aiohttp & websockets"
+        return (
+            Embed(
+                title="Pincer - 0.6.4",
+                description=(
+                    "🚀 An asynchronous python API wrapper meant to replace"
+                    " discord.py\n> Snappy discord api wrapper written "
+                    "with aiohttp & websockets"
+                ),
             )
-        ).add_field(
-            name="**Github Repository**",
-            value="> https://github.com/Pincer-org/Pincer"
-        ).set_thumbnail(
-            url="https://pincer.dev/img/icon.png"
-        ).set_image(
-            url=(
-                "https://repository-images.githubusercontent.com"
-                "/400871418/045ebf39-7c6e-4c3a-b744-0c3122374203"
+            .add_field(
+                name="**Github Repository**",
+                value="> https://github.com/Pincer-org/Pincer",
+            )
+            .set_thumbnail(url="https://pincer.dev/img/icon.png")
+            .set_image(
+                url=(
+                    "https://repository-images.githubusercontent.com"
+                    "/400871418/045ebf39-7c6e-4c3a-b744-0c3122374203"
+                )
             )
         )
 
