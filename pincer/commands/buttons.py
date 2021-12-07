dcf4624e8c80a729197cfb75459039b14571c9b4
Lunarmagpie <Bambolambo0@gmail.com>
2021-12-07 16:55:29 -0500

    :sparkles: added button handler

---
+++ pincer/commands/buttons.py
@@ -0,0 +1,36 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+from typing import Callable, Dict
+
+from pincer.utils.types import Singleton
+
+
+def hash_button_id(_id):
+    return str(hash(id))
+
+
+# I'm ignoring the name collision so `button(id="str")` is valid code
+def button(func=None, id=None, hash_id=True):
+
+    def wrap(id):
+        id = hash_button_id(id)
+        ButtonHandler().register_id(id, func)
+        return func
+
+    if func is None:
+        return wrap
+
+    return wrap(id)
+
+
+class ButtonHandler(metaclass=Singleton):
+
+    def __init__(self) -> None:
+        self.buttons: Dict[str, Callable] = {}
+
+    def register_id(self, _id: str, func: Callable):
+        self.buttons[_id] = func
+
+    def __getitem__(self, _id):
+        return self.buttons[_id]
