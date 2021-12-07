1964bee5747aee06b722b2a631df3ce11133921e
sigmanificient <sigmanificient@gmail.com>
2021-12-07 23:35:14 +0100

    :rotating_light: Returning Any from function declared to return "Optional[T]"

--- pincer/utils/extraction.py
+++ pincer/utils/extraction.py
@@ -1,22 +1,23 @@
 # Copyright Pincer 2021-Present
 # Full MIT License can be found in `LICENSE` at the project root.
 
-from typing import Any, Optional, Protocol, TypeVar
+from typing import Any, Optional, Protocol, TypeVar, Union
 
 T = TypeVar("T")
+V = TypeVar("V")
 
 
 class GetItem(Protocol):
     """Represents a class which implements the __getitem__ property."""
-    def __getitem__(self, key: int) -> Any:
-        return ...
+    def __getitem__(self, key: int) -> V:
+        ...
 
 
 def get_index(
         collection: GetItem,
         index: int,
         fallback: Optional[T] = None
-) -> Optional[T]:
+) -> Union[V, Optional[T]]:
     """Gets an item from a collection through index.
     Allows you to provide a fallback for if that index is out of bounds.
 
