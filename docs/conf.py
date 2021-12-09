90174d162f2971fed2f858bea3c09a6dec560714
RPS <vincentbusiness55@gmail.com>
2021-12-09 09:49:06 +0800

    src

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../src'))
 sys.path.append(os.path.abspath("../.."))
 sys.path.append(os.path.abspath('extensions'))
 
