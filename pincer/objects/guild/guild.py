3e161b43983efe41f707ce8619b2102f1f2aef1a
sigmanificient <sigmanificient@gmail.com>
2021-12-12 20:49:46 +0100

    :fire: optimized imports

--- pincer/objects/guild/guild.py
+++ pincer/objects/guild/guild.py
@@ -3,23 +3,19 @@
 
 from __future__ import annotations
 
-from collections import AsyncIterator
 from dataclasses import dataclass, field
 from enum import IntEnum
 from typing import AsyncGenerator, overload, TYPE_CHECKING
 
 from aiohttp import FormData
 
-from .invite import Invite
 from .channel import Channel
 from .invite import Invite
-from ..message.emoji import Emoji
 from ..message.file import File
 from ...exceptions import UnavailableGuildError
 from ...utils.api_object import APIObject
 from ...utils.conversion import construct_client_dict, remove_none
 from ...utils.types import MISSING
-
 
 if TYPE_CHECKING:
     from ...utils.types import JsonDict
@@ -43,7 +39,7 @@ if TYPE_CHECKING:
     from ..voice.region import VoiceRegion
     from ..events.presence import PresenceUpdateEvent
     from ..message.emoji import Emoji
-    from ..message.sticker import Sticker, StickerPack
+    from ..message.sticker import Sticker
     from ..user.voice_state import VoiceState
     from ...client import Client
     from ...utils.timestamp import Timestamp
