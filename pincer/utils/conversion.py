d416e800472b06e6d30686fadf8b3cf671d4a9ae
Endercheif <45527309+Endercheif@users.noreply.github.com>
2021-12-17 12:42:35 -0800

    :sparkles: Added tuple support to `remove_none`

--- pincer/utils/conversion.py
+++ pincer/utils/conversion.py
@@ -10,7 +10,7 @@ from .types import T, MISSING
 
 if TYPE_CHECKING:
     from ..client import Client
-    from typing import Any, Callable, Dict, List, Optional, Set, Union
+    from typing import Any, Callable, Dict, List, Optional, Set, Union, Tuple
 
 
 def construct_client_dict(client: Client, data: Dict) -> Dict:
@@ -29,22 +29,24 @@ def construct_client_dict(client: Client, data: Dict) -> Dict:
     return {**data, "_client": client}
 
 
-def remove_none(obj: Union[List, Dict, Set]) -> Union[List, Dict, Set]:
+def remove_none(obj: Union[List, Dict, Set, Tuple]) -> Union[List, Dict, Set, Tuple]:
     """
     Removes all ``None`` values from a list, dict or set.
 
     Parameters
     ----------
-    obj : Union[List, Dict, Set]
-        The list, dict or set to remove ``None`` values from.
+    obj : Union[List, Dict, Set, Tuple]
+        The list, dict, set or tuple to remove ``None`` values from.
 
     Returns
     -------
-    Union[List, Dict, Set]
-        The list, dict or set without ``None`` values.
+    Union[List, Dict, Set, Tuple]
+        The list, dict, set or tuple, without ``None`` values.
     """
     if isinstance(obj, list):
         return [i for i in obj if i is not None]
+    elif isinstance(obj, list):
+        return tuple(i for i in obj if i is not None)
     elif isinstance(obj, set):
         return obj - {None}
     elif isinstance(obj, dict):
