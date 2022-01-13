fa7d397cd41057faefde52ce259d23c4afd1f63f
Lunarmagpie <Bambolambo0@gmail.com>
2022-01-12 21:13:19 -0500

    :bug: fix unimported typehints

--- pincer/utils/shards.py
+++ pincer/utils/shards.py
@@ -1,6 +1,15 @@
 # Copyright Pincer 2021-Present
 # Full MIT License can be found in `LICENSE` at the project root.
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Union
+    from .snowflake import Snowflake
+
+
 def calculate_shard_id(guild_id: Union[Snowflake, int], num_shards: int) -> int:
     """Calculates the shard receiving the events for a specified guild
 
